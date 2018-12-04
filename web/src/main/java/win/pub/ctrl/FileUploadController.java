074a13c088496e5176c68dcf1388530f36d1fca2
awinj <1273638029@qq.com>
2018-12-04 23:27:30 +0800

    文件上传demo

--- web/src/main/java/win/pub/ctrl/FileUploadController.java
+++ web/src/main/java/win/pub/ctrl/FileUploadController.java
@@ -3,10 +3,18 @@ package win.pub.ctrl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import win.pub.vo.Result;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
 
 /**
  * Created by aWin on 2018-12-04.
@@ -18,8 +26,15 @@ import javax.servlet.http.HttpServletResponse;
 public class FileUploadController {
 
     @RequestMapping(value = "upload",method = RequestMethod.POST)
-    public Result upload(HttpServletRequest request, HttpServletResponse response)
-    {
+    public Result upload(MultipartFile file, HttpServletRequest request) throws IOException {
+         String path =request.getSession().getServletContext().getRealPath("/"); //服务器地址
+        if (!file.isEmpty()) {
+            // 文件保存路径
+            String filePath = path + file.getOriginalFilename();
+            // 转存文件
+
+            file.transferTo(new File(filePath));
+        }
         return null;
     }
 }
